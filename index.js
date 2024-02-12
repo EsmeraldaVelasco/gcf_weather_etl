9cf6c81e9567a5965c79fdbe90462feb88ded9c4
Esmeralda Velasco <esvelasc@iu.edu>
2024-02-12 04:20:18 +0000

    End of v4- couldn't connect to bigquery

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const {Storage}=require('@google-cloud/storage');
+const {BigQuery}= require('@google-cloud/bigquery');
 const csv=require('csv-parser');
+
+const bq=new BigQuery();
+const datasetId='weather_dev';
+const tableId='weather';
 
 exports.readObservation = (file, context) => {
     // console.log(`  Event: ${context.eventId}`);
@@ -23,6 +28,12 @@ exports.readObservation = (file, context) => {
     .on('end', ()=>{
         //Handle the end of CSV
         console.log('End!');
+        try{
+           writeToBq(row);
+        }
+        catch(error){
+            console.error('Error')
+        }
     })
 
 
@@ -30,12 +41,31 @@ exports.readObservation = (file, context) => {
 }
 
 //Helper function
-
 function printDict(row){
     for (let key in row){
+        if(row[key] == '-9999'){
+            row[key]=null;
+        }
+        if(key=='airtemp'|| key=='dewpoint' ||key=='pressure' || key=='windspeed'|| key=='precip1hour' || key=='precip6hour'){
+            row[key]/10;
+        }
+
         console.log(key + ':' + row[key]);
         //console.log(`${key} : ${row[key]}`)
-    }
+}
+}
 
+
+//Create a helper function that writes to BQ
+//Function must be asynchronous 
+async function writeToBq(row) {
+    await bq
+    .dataset(datasetId)
+    .table(tableId)
+    .insert(row)
+    .then( ()=>{
+        row.forEach( (row) =>{console.log(`Inserted: ${row}`)})
+    })
+    .catch( (err)=>{console.error(`ERROR: ${err}`)})
 }
 
