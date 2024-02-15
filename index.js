14511b541074114982abc32f8782ae74dd81437e
Esmeralda Velasco <esvelasc@iu.edu>
2024-02-15 09:06:57 +0000

    Fixed

--- index.js
+++ index.js
@@ -6,14 +6,23 @@ const bq=new BigQuery();
 const datasetId='weather_dev';
 const tableId='weather';
 
+
 exports.readObservation = (file, context) => {
     // console.log(`  Event: ${context.eventId}`);
     // console.log(`  Event Type: ${context.eventType}`);
     // console.log(`  Bucket: ${file.bucket}`);
     // console.log(`  File: ${file.name}`);
 
+    
+   
     const gcs=new Storage();
     const dataFile=gcs.bucket(file.bucket).file(file.name);
+
+    const fileName=file.name;
+    const print=[];
+
+
+
     dataFile.createReadStream()
     .on('error', ()=>{
         //Handle an error
@@ -23,48 +32,57 @@ exports.readObservation = (file, context) => {
     .on('data', (row)=>{
         //Log row data
         //console.log(row);
+        //printDict(row)
+        print.station=fileName;
+        console.log(`${print.station}`);
         printDict(row);
+        writeToBq(row);
+
     })
     .on('end', ()=>{
         //Handle the end of CSV
         console.log('End!');
-        try{
-           writeToBq(row);
-        }
-        catch(error){
-            console.error('Error')
-        }
+    
     })
 
-
-
 }
+
+
+
 
 //Helper function
 function printDict(row){
     for (let key in row){
-        if(row[key] == '-9999'){
+
+        if(row[key] =='-9999.0'){
             row[key]=null;
         }
-        if(key=='airtemp'|| key=='dewpoint' ||key=='pressure' || key=='windspeed'|| key=='precip1hour' || key=='precip6hour'){
-            row[key]/10;
+
+        if (['airtemp', 'dewpoint', 'pressure', 'windspeed', 'precip1hour', 'precip6hour'].includes(key) && !null) {
+             row[key] = parseFloat(row[key]) / 10;
         }
+
 
         console.log(key + ':' + row[key]);
         //console.log(`${key} : ${row[key]}`)
-}
+        
+    }
+
+
+    
 }
 
 
 //Create a helper function that writes to BQ
 //Function must be asynchronous 
 async function writeToBq(row) {
+
     await bq
     .dataset(datasetId)
     .table(tableId)
     .insert(row)
     .then( ()=>{
-        row.forEach( (row) =>{console.log(`Inserted: ${row}`)})
+        console.log('added')
     })
     .catch( (err)=>{console.error(`ERROR: ${err}`)})
 }
